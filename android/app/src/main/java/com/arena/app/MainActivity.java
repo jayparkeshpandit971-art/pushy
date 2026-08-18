fe9693fe567520b0ec5116737a977fbe0a001ae2
jayparkeshpandit971-art <jayparkeshpandit971@gmail.com>
2026-08-18 08:42:56 +0000

    Fix FCM native token

--- android/app/src/main/java/com/arena/app/MainActivity.java
+++ android/app/src/main/java/com/arena/app/MainActivity.java
@@ -1,5 +1,22 @@
 package com.arena.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+            if (task.isSuccessful() && task.getResult() != null) {
+                String token = task.getResult();
+                getBridge().getWebView().evaluateJavascript(
+                    "window._nativeFcmToken = '" + token + "'; " +
+                    "if(window._onFcmToken) window._onFcmToken('" + token + "');",
+                    null
+                );
+            }
+        });
+    }
+}
